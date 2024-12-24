d8be5c3fbc12e0c63f4d675c0a8c4f4b5ad06fa4
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2024-12-24 08:00:24 -0300

    Correções e melhorias.

--- src/main/java/guru/learningjournal/examples/kafka/jsonposgen/JsonPosGeneratorApplication.java
+++ src/main/java/guru/learningjournal/examples/kafka/jsonposgen/JsonPosGeneratorApplication.java
@@ -1,13 +1,38 @@
 package guru.learningjournal.examples.kafka.jsonposgen;
 
+import guru.learningjournal.examples.kafka.jsonposgen.services.KafkaProducerService;
+import guru.learningjournal.examples.kafka.jsonposgen.services.datagenerator.InvoiceGenerator;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JsonPosGeneratorApplication {
+@Log4j2
+public class JsonPosGeneratorApplication implements ApplicationRunner {
+
+	@Value("${application.configs.invoice.count}")
+	private int INVOICE_COUNT;
+
+	@Autowired
+	private KafkaProducerService producerService;
+
+	@Autowired
+	private InvoiceGenerator invoiceGenerator;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsonPosGeneratorApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		for (int i = 0; i < INVOICE_COUNT; i++) {
+			this.producerService.sendMessage(this.invoiceGenerator.getNextInvoice());
+			Thread.sleep(1000);
+		}
 	}
 
 }
--- src/main/java/guru/learningjournal/examples/kafka/jsonposgen/services/datagenerator/ProductGenerator.java
+++ src/main/java/guru/learningjournal/examples/kafka/jsonposgen/services/datagenerator/ProductGenerator.java
@@ -1,49 +1,55 @@
 package guru.learningjournal.examples.kafka.jsonposgen.services.datagenerator;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import guru.learningjournal.examples.kafka.jsonposgen.model.LineItem;
+import java.io.File;
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import guru.learningjournal.examples.kafka.jsonposgen.model.LineItem;
 
 @Service
 public class ProductGenerator {
 
-    private final Random random;
+	private final Random random;
 
-    private final Random qty;
+	private final Random qty;
 
-    private final LineItem[] products;
+	private final LineItem[] products;
 
-    public ProductGenerator() {
-        String DATAFILE = "src/main/resources/products.json";
+	public ProductGenerator() {
+		String DATAFILE = "src/main/resources/data/products.json";
 
-        ObjectMapper objectMapper = new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
 
-        this.random = new Random();
+		this.random = new Random();
 
-        this.qty = new Random();
+		this.qty = new Random();
 
-        try {
-            this.products = objectMapper.readValue(ProductGenerator.class.getResourceAsStream(DATAFILE), LineItem[].class);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+		try {
+			// this.products =
+			// objectMapper.readValue(ProductGenerator.class.getResourceAsStream(DATAFILE),
+			// LineItem[].class);
+			this.products = objectMapper.readValue(new File(DATAFILE), LineItem[].class);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    private int getIndex() {
-        return this.random.nextInt(100);
-    }
+	private int getIndex() {
+		return this.random.nextInt(100);
+	}
 
-    private int getQuantity() {
-        return this.qty.nextInt(2) + 1;
-    }
+	private int getQuantity() {
+		return this.qty.nextInt(2) + 1;
+	}
 
-    public LineItem getNextProduct() {
-        LineItem lineItem = this.products[getIndex()];
-        lineItem.setItemQty(getQuantity());
-        lineItem.setTotalValue(lineItem.getItemPrice() + lineItem.getItemQty());
-        return lineItem;
-    }
+	public LineItem getNextProduct() {
+		LineItem lineItem = this.products[getIndex()];
+		lineItem.setItemQty(getQuantity());
+		lineItem.setTotalValue(lineItem.getItemPrice() + lineItem.getItemQty());
+		return lineItem;
+	}
 
 }
